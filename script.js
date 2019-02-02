7d3687359fba9f6b2ffd3e379455926f01cf042a
Astghik Khachatryan <astghikhachatrian@gmail.com>
2019-02-02 16:54:05 +0400

    carcase, js code

---
+++ script.js
@@ -0,0 +1,28 @@
+let indexDiff = 0;
+
+while (indexDiff === 0) {
+	let player1 = prompt('1st player.', 'paper');
+	let player2 = prompt('2nd player.', 'scissors');
+	let player1Array = ['paper', 'rock', 'scissors'];
+	let player2Array = ['paper', 'rock', 'scissors'];
+	let player1Index = player1Array.indexOf(player1);
+	let player2Index = player2Array.indexOf(player2);
+	
+	if (player1Index !== -1 && player2Index !== -1) {
+		indexDiff = player1Index - player2Index;
+		
+		if (indexDiff === 0) {
+			alert('Draw! Enter new values.');
+		}
+    
+	} else {
+		indexDiff = 0;
+	}
+  
+}
+	
+if (indexDiff === -1 || indexDiff === 2) {
+	alert('1st  player wins!');
+} else {  	 // if (indexDiff === -2 || indexDiff === 1)
+	alert('2nd player wins!');
+}
