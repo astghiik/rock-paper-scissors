7f59d854978064f278e829d5a9872cba89e5b609
Astghik Khachatryan <astghikhachatrian@gmail.com>
2019-02-02 17:27:27 +0400

    simple code

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let indexDiff = 0;
 while (indexDiff === 0) {
 	let player1 = prompt('1st player.', 'paper');
 	let player2 = prompt('2nd player.', 'scissors');
-	let player1Array = ['paper', 'rock', 'scissors'];
-	let player2Array = ['paper', 'rock', 'scissors'];
-	let player1Index = player1Array.indexOf(player1);
-	let player2Index = player2Array.indexOf(player2);
+	let player1Tools = ['paper', 'rock', 'scissors'];
+	let player2Tools = ['paper', 'rock', 'scissors'];
+	let player1Index = player1Tools.indexOf(player1);
+	let player2Index = player2Tools.indexOf(player2);
 	
 	if (player1Index !== -1 && player2Index !== -1) {
 		indexDiff = player1Index - player2Index;
